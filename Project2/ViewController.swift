1ad281964103c3f8b1de1e88e3d742665b226032
Deepak Bora <appinventiv@Appinventivs-Mac-mini1111.local>
2018-03-12 18:54:37 +0530

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Appinventiv on 12/03/18.
+//  Copyright © 2018 Appinventiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Helloo")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
